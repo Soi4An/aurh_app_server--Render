1db072e59ebec3dca7a097031d8a7b2e87cf32c4
Antonio S <soi4.toxa@ukr.net>
2024-04-11 16:33:40 +0300

    changed db

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,14 +4,20 @@ require('dotenv/config');
 
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize({
+const db = new Sequelize(process.env.POSTGRES_URL, {
   dialect: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: Number(process.env.POSTGRES_PORT) || 5432,
-  database: process.env.POSTGRES_DB || 'postgres',
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD,
-  logging: false
+  dialectModule: pg,
+  logging: false,
 });
+
+// const db = new Sequelize({
+//   dialect: 'postgres',
+//   host: process.env.POSTGRES_HOST || 'localhost',
+//   port: Number(process.env.POSTGRES_PORT) || 5432,
+//   database: process.env.POSTGRES_DB || 'postgres',
+//   username: process.env.POSTGRES_USER || 'postgres',
+//   password: process.env.POSTGRES_PASSWORD,
+//   logging: false
+// });
 
 module.exports = { db };
